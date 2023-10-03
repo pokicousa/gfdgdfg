363d364a508e6287dde1192709bdaeb53089178f
pokicousa <umityurdakul@gmail.com>
2023-10-03 12:36:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,104 +1,69 @@
-///bu
-
-const posts = [
+const quotes = [
   {
     id: 1,
-    quote: 'Büyük şeyler küçük şeylerin bir araya gelmesidir.',
-    author: 'Van Gogh',
+    quote:
+      'Odağını, dış koşul ve olaylara tepki vermek üzerinden çektiğinde, yeni açılımlara da fırsat vermiş olacaksın...',
+    author: 'AUSEY',
   },
   {
     id: 2,
-    quote:
-      'Açıklamalarla vaktini harcama. İnsanlar sadece duymak istediklerini duyarlar.',
-    author: 'Paulo Coelho',
+    quote: 'Yaşamdaki en güçlü rehberin kendi gerçekliğindir. Ona sahip çık...',
+    author: 'AUSEY',
   },
   {
     id: 3,
-    quote: 'Her bildiğini söyleme, her söylediğini bil.',
-    author: 'Clavdius',
+    quote: 'Hala zaman gibi eşsiz bir hazinen var. O zaman şimdi başla!..',
+    author: 'AUSEY',
   },
   {
     id: 4,
-    quote: 'Savaşın iyisi, barışın kötüsü yoktur.',
-    author: 'Benjamin Franklin',
+    quote: 'Yaşamını kişiselleştir. Olayları, durumları değil...',
+    author: 'AUSEY',
   },
   {
     id: 5,
-    quote: 'Mutluluğu tatmanın tek çaresi, onu paylaşmaktır.',
-    author: 'Byron',
+    quote:
+      'Yaşamı ve yaşayanları zerafetle karşılamak insan ruhuna iyi gelir...',
+    author: 'AUSEY',
   },
   {
     id: 6,
-    quote: 'Hayatta hep mutlu olursam, Hayalini kuracak neyim kalır.',
-    author: 'Dostoyevski',
+    quote: 'Yolun süresine değil sürecine odaklan...',
+    author: 'AUSEY',
   },
   {
     id: 7,
-    quote:
-      'İnsanların seni çok sevdiği zaman, onların işine en çok yaradığın zamandır',
-    author: 'Bukowski',
+    quote: 'Kendini keşfetmenin önemli bir yolu hayatı daha çok yaşamaktır...',
+    author: 'AUSEY',
   },
   {
     id: 8,
-    quote:
-      'Bir kalbi kırdıktan sonra gelen özür, doyduktan sonra sofraya gelen tuz gibidir. İhtiyaç kalmaz!',
-    author: 'Pablo Neruda',
+    quote: 'Ego ile idrak ters orantılı gelişir...',
+    author: 'AUSEY',
   },
   {
     id: 9,
-    quote:
-      'Ya susmak ya da suskunluktan daha kıymetli bir söz söylemek gerekir.',
-    author: 'Pisagor',
+    quote: 'Ego iyi niyeti bozar...',
+    author: 'AUSEY',
   },
   {
     id: 10,
-    quote: 'Gülmek için mutlu olmayı beklemeyin belki de gülmeden ölürsünüz.',
-    author: 'Victor Hugo',
+    quote:
+      'Kendi gerçekliğini yaratmanın en önemli aracı, güdülerinin üzerindeki özgür iradendir...',
+    author: 'AUSEY',
   },
   {
     id: 11,
-    quote: 'Kendini yanlış hikayede bulursan ayrıl.',
-    author: 'Mo Willems',
+    quote:
+      'Yarın almayı beklediğin bugün alabileceğinden daha büyük olmayabilir...',
+    author: 'AUSEY',
   },
 ];
-// To keep the published post visible on a web page for 24 hours, you can store the post content along with a timestamp indicating when it was published. Then, you can check the timestamp whenever someone visits the web page to determine if the post is still valid (i.e., if it has been less than 24 hours since it was published). Here's an example of how you can modify the code to achieve this:
 
-let currentPost = null;
+const randomIndex = Math.floor(Math.random() * quotes.length);
 
-function publishPost() {
-  const currentDate = new Date();
-  const postIndex = currentDate.getDate() % posts.length;
-  // const randomPostIndex = Math.floor(Math.random() * postIndex);
-  currentPost = {
-    content: posts[postIndex],
-    timestamp: currentDate.getTime(), // Store the timestamp when the post was published
-  };
-  // console.log('Publishing post: ', currentPost.content);
-  // console.log(currentDate);
-}
+const randomObject = quotes[randomIndex];
 
-function displayPost() {
-  // Check if there is a current post and if it has been less than 24 hours since it was published
-  if (
-    currentPost &&
-    new Date().getTime() - currentPost.timestamp < 24 * 60 * 60 * 1000
-  ) {
-    // Display the current post on the web page
-    document.getElementById('quote-text').textContent =
-      currentPost.content.quote;
-    document.getElementById('author').textContent = currentPost.content.author;
-  } else {
-    // If the post is no longer valid, publish a new post
-    publishPost();
-    displayPost(); // Update the displayed post
-  }
-}
-
-// Call the displayPost function when the page loads to display the initial post
-displayPost();
-
-// Schedule the publishPost function to run every day at a specific time (for example, at 12:00 AM)
-setInterval(function () {
-  publishPost();
-  displayPost(); // Update the displayed post
-}, 24 * 60 * 60 * 1000); // Run every 24 hours
+document.getElementById('quote-text').textContent = randomObject.quote;
+document.getElementById('author').textContent = randomObject.author;
+// console.log(randomObject.quote);
